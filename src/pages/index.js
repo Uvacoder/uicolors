196338790cbcdde7b4744b91cbd4960dc3add9a1
Subho <contact.subhoghosh@gmail.com>
2021-02-20 17:43:10 +0530

    Initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Get UI Colors</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="font-bold text-red-500 text-xl">Hello World</h1>
+    </div>
+  )
+}
