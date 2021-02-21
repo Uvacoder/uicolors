d9d00073dcc9d18a240e171b1abaeecc833f55fd
Subho <contact.subhoghosh@gmail.com>
2021-02-21 11:51:49 +0530

    Refactor(SideBar.js): Remove comments

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,11 +2,6 @@ function SideBar() {
 
     return (
         <nav className="flex fixed bottom-0 h-16 w-screen bg-gray-100 sm:flex-col sm:w-16 sm:h-screen sm:left-0">
-          {/* <div className="hidden sm:h-16 sm:bg-purple-500 sm:flex sm:justify-center sm:items-center sm:hover:bg-purple-600 sm:cursor-pointer">
-            <svg className="text-white w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
-            </svg>
-          </div> */}
           <div className="hidden sm:h-16 sm:text-white sm:font-bold sm:bg-gradient-to-r sm:from-purple-400 sm:to-purple-700 sm:flex sm:justify-center sm:items-center">
               UI
           </div>
@@ -29,9 +24,6 @@ function SideBar() {
                 <path d="M253.487 57.1976H343.436L171.723 392.158V245.107L253.487 57.1976Z" fill="#AEDDFF"/>
                 </svg>
               </div>
-              {/* <div className="h-10 w-10 hover:bg-gray-200 rounded cursor-pointer flex justify-center items-center">
-                <svg className="bright-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="40" height="32" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fillRule="evenodd" clipRule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>
-              </div> */}
             </div>
             <div className="flex justify-between items-center w-2/6 sm:w-auto sm:block sm:space-y-2">
               <div style={{fontSize: '10px'}} className="h-10 w-10 mx-auto cursor-pointer rounded flex justify-center items-center hover:bg-gray-200">RGB</div>
