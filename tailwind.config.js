39fff59490d0557c7daeba44204d933e20d42b6e
Subho <contact.subhoghosh@gmail.com>
2021-02-21 12:28:13 +0530

    Update(tailwind.config.js): Enable Dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     colors: {
       black: colors.black,
