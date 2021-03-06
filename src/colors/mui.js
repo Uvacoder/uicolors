3d17f419c4d8375ce29cf882acb2db2d1a5901b1
Subho <contact.subhoghosh@gmail.com>
2021-03-06 19:12:12 +0530

    Refactor(mui.js): remove comment

--- src/colors/mui.js
+++ src/colors/mui.js
@@ -1,5 +1,4 @@
 const colors = require('@material-ui/core/colors');
 const returnImportantColors = require('../utils/returnImportantColors');
 
-console.log(returnImportantColors(colors));
 module.exports = returnImportantColors(colors).reverse();
