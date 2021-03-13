6868fe5f56c1f841321faa883c1249585810599c
Subho <contact.subhoghosh@gmail.com>
2021-03-13 19:53:56 +0530

    feat: loading and success state of contact form

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -23,16 +23,28 @@ const encode = (data) => {
 function SideBar({ isDark, setIsDark, currentTab, setCurrentTab, showRGB, setShowRGB, isExpanded, setIsExpanded, isOldView, setIsOldView, showSettings, setShowSettings }) {
   const [showForm, setShowForm] = useState(false);
   const [formData, setFormData] = useState({name: '', email: '', message: ''});
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [submitInProgress, setSubmitInProgress] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if(!submitInProgress) {
+      setSubmitInProgress(true);
+    }
     
     fetch("/", {
       method: 'POST',
       headers: { "Content-Type": "application/x-www-form-urlencoded"},
       body: encode({"form-name": "contact", ...formData})
     })
-    .then(() => console.log('success'))
+    .then(() => {
+      console.log('success');
+      if(!submitSuccess) {
+        setSubmitSuccess(true);
+        setFormData({});
+      }
+    })
     .catch((err) => console.log(err))
   }
 
@@ -76,22 +88,38 @@ function SideBar({ isDark, setIsDark, currentTab, setCurrentTab, showRGB, setSho
         </div>
         <div className="flex justify-around items-center w-1/5 sm:w-auto sm:block sm:space-y-2">
           <div className={`${showSettings ? 'flex' : 'hidden'} justify-around items-center absolute bottom-16 left-1/2 transform -translate-x-1/2 space-x-3 sm:space-x-0 sm:transform-none px-3 py-2 rounded-t-md sm:p-0 bg-gray-100 dark:bg-gray-800 sm:static sm:w-auto ${showSettings ? 'sm:block' : 'hidden'} sm:space-y-2`}>
-            <div onClick={(e) => {e.target === e.currentTarget && setShowForm(false)}} className={`${showForm ? 'flex' : 'hidden'} justify-center items-center fixed z-20 inset-0 h-screen w-screen bg-gray-700 bg-opacity-70`}>
+            <div onClick={(e) => {e.target === e.currentTarget && setShowForm(false); submitInProgress && setSubmitInProgress(false); submitSuccess && setSubmitSuccess(false)}} className={`${showForm ? 'flex' : 'hidden'} justify-center items-center fixed z-20 inset-0 h-screen w-screen bg-gray-700 bg-opacity-70`}>
               <form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" className="w-full max-w-lg bg-white rounded px-10 py-8">
                 <input type="hidden" name="form-name" value="contact" />
-                <label>
-                  <div className="text-gray-600 font-bold">Name<span className="text-red-500 ml-1 text-sm">*</span></div>
-                  <input onChange={handleChange} value={formData.name} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="text" name="name" required/>
-                </label>
-                <label className="mt-4 block">
-                  <div className="text-gray-600 font-bold">Email<span className="text-red-500 ml-1 text-sm">*</span></div>
-                  <input onChange={handleChange} value={formData.email} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="email" name="email" required/>
-                </label>
-                <label className="mt-4 block">
-                  <div className="text-gray-600 font-bold">Message<span className="text-red-500 ml-1 text-sm">*</span></div>
-                  <textarea onChange={handleChange} value={formData.message} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="text" name="message" rows="3"/>
-                </label>
-                <input className="block mt-4 w-full bg-gray-600 py-3 rounded font-bold text-white cursor-pointer hover:bg-gray-800" type="submit" value="Submit"/>
+                <div className={`${submitSuccess ? 'hidden' : 'block'}`}>
+                  <label>
+                    <div className="text-gray-600 font-bold">Name<span className="text-red-500 ml-1 text-sm">*</span></div>
+                    <input onChange={handleChange} value={formData.name} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="text" name="name" required/>
+                  </label>
+                  <label className="mt-4 block">
+                    <div className="text-gray-600 font-bold">Email<span className="text-red-500 ml-1 text-sm">*</span></div>
+                    <input onChange={handleChange} value={formData.email} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="email" name="email" required/>
+                  </label>
+                  <label className="mt-4 block">
+                    <div className="text-gray-600 font-bold">Message<span className="text-red-500 ml-1 text-sm">*</span></div>
+                    <textarea onChange={handleChange} value={formData.message} className="mt-1 w-full border rounded px-4 py-2 focus:outline-none focus:border-gray-500 text-gray-700" type="text" name="message" rows="3"/>
+                  </label>
+                  <button type="submit" className="flex justify-center items-center space-x-2 mt-4 w-full bg-gray-600 py-3 rounded font-bold text-white cursor-pointer hover:bg-gray-800 focus:outline-none" type="submit">
+                    {!submitInProgress && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-send h-5 w-5"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>}
+                    {submitInProgress && <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>}
+                    <span>Submit</span>
+                  </button>
+                </div>
+                {submitSuccess && 
+                <div className="flex justify-center items-center space-x-1">
+                  <svg className="h-10 w-10 text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                  </svg>
+                  <span className="text-green-600 text-3xl font-bold">Sucess</span>
+                </div>}
               </form>
             </div>
             <button onClick={() => setShowForm(true)} className={`${btnCls({active: false, isExpanded})} hidden sm:flex`}>
